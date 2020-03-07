0060a03558f93b30529d960b00b0015449dbf0f4
zenith1604 <zenith.gg@gmail.com>
2020-03-07 12:33:14 +0700

    add aof resource

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/resource/index.html');
 })
 
+app.get('/aof',(req,res)=>{
+    res.sendFile(__dirname+'/resource/aof.html');
+})
+
 //bodyparser middleware
 app.use(bodyParser.json());
 
