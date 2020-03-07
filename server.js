36a797dcd2e1465280e31d25ab3c97ce0c835355
zenith1604 <zenith.gg@gmail.com>
2020-03-07 13:13:57 +0700

    add page1 resource

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/resource/index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/resource/index.html');
 })
 
-app.get('/aof',(req,res)=>{
-    res.sendFile(__dirname+'/resource/aof.html');
+app.get('/aof', (req, res) => {
+    res.sendFile(__dirname + '/resource/aof.html');
+    // var gulcanigga = ["ewfjwlkefjlkfwklejflkwejf","gergergergerrrrrrrrrrrr"] ;
+    // var gul = {
+    //     "name":"boss",
+    //     "pet":["black man","plan"]
+    // }
+    // console.log("hello");
 })
-
-//bodyparser middleware
+app.get('/page1', (req, res) => {
+        res.sendFile(__dirname + '/resource/page1.html');
+    })
+    //bodyparser middleware
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, ()=> console.log(`server start on port ${port}`));
+app.listen(port, () => console.log(`server start on port ${port}`));
